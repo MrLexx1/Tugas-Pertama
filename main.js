4af0f4882686329f57c2ed955e7db0ed8c370bac
MrLexx1 <muhilman606@gmail.com>
2024-10-20 08:41:21 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// TUGAS PERTAMA
+
+let angka = prompt("input nilai : ");
+
+angka = Number(angka);
+
+if (angka % 2 === 0) {
+  console.log("angka ini adalah genap");
+} else if (angka % 2 !== 0) {
+  console.log("angka ini adalah ganjil");
+}
+
